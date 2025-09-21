e3b0898cff84ee72467b94ec87e7772eb158e8d6
Fuad Mohammed <128188086+Daufm@users.noreply.github.com>
2025-09-21 22:27:55 +0300

    Time: 8 ms (70.28%), Space: 12.3 MB (98.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x = str(x)
-        y = x[::-1]
-        if x == y:
-            return True
-        return False
+        if x < 0 :
+            return False
+        original = x
+        reverse = 0
+        while x != 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x=x/10
+        return  original == reverse
+
+
+        
         
